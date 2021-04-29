36d192170e20e1d13bf9f61aa59331f731e8652a
Coni <constantinescu.andrei03@gmail.com>
2021-04-29 11:43:17 +0300

    Change readme

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -12,10 +12,10 @@ export class DataService {
   constructor() {
     if (localStorage.getItem('patients') === null) {
       localStorage.setItem('patients',JSON.stringify([
-        {firstName : 'Georgescu', lastName : 'Andrei', birthDate:'3/07/2000', gender: 'M', CNP: 1356234677975, phoneNumber : '0740678549'},
-        {firstName : 'Popescu', lastName : 'Georgiana', birthDate:'14/02/1994', gender: 'F', CNP: 4321264647935, phoneNumber : '0756898023'},
-        {firstName : 'Andrei', lastName : 'Ionut', birthDate:'15/10/1970', gender: 'M', CNP: 1152537697475, phoneNumber : '0768752450'},
-        {firstName : 'Mihaiu', lastName : 'Andreea', birthDate:'8/09/2002', gender: 'F', CNP: 1358205616975, phoneNumber : '0739492786'}
+        {firstName : 'Georgescu', lastName : 'Andrei', birthDate:'2000-07-03', gender: 'M', CNP: 1356234677975, phoneNumber : '0740678549'},
+        {firstName : 'Popescu', lastName : 'Georgiana', birthDate:'1994-02-14', gender: 'F', CNP: 4321264647935, phoneNumber : '0756898023'},
+        {firstName : 'Andrei', lastName : 'Ionut', birthDate:'1970-10-15', gender: 'M', CNP: 1152537697475, phoneNumber : '0768752450'},
+        {firstName : 'Mihaiu', lastName : 'Andreea', birthDate:'2002-08-08', gender: 'F', CNP: 1358205616975, phoneNumber : '0739492786'}
     ]));
   } 
     this.patients=JSON.parse(localStorage.getItem('patients'));
